10c6e75698da04097207ebc691b33158d399f8c7
RajkumarGithub1 <104200373+RajkumarGithub1@users.noreply.github.com>
2022-05-10 17:27:44 +0530

    Update CreateUserTest.java

--- src/test/java/MavenProjectExample/CreateUserTest.java
+++ src/test/java/MavenProjectExample/CreateUserTest.java
@@ -7,7 +7,7 @@ public class CreateUserTest
 	@Test(groups="smoke")
 	public void createUser()
 	{
-		
+		System.out.println("job done after changes did");
 		System.out.println("Create User test Pass");
 	}
 }
